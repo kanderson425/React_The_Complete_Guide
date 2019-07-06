e9ccd425c9a69c7e3682adecee578e53f7b4505e
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-06 14:27:41 -0500

    Adds the person component from lecture #31

---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//Using ES6 - this is how we will create this component using const
+//We will use the arrow function courtesy of ES6, this is better form
+
+const person = () => {
+    return <p>I'm a Person!</p>
+
+}
+
+export default person;
