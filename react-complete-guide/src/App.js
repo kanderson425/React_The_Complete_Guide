654f3e8170648a9de87b24a7e1b05e8c02a09771
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-07 14:52:39 -0500

    Changes class based App component to a functional component and applies hooks method for state changing functionality

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,39 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26}
-    ]
-  }
+const App = (props) => {
+  const [ personsState, setPersonsState] = useState(
+    {
+      persons: [
+        {name: 'Max', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 26}
+      ],
+      otherState: 'some other value'
+    }
+  );
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // console.log('Was clicked');
-    // DON'T DO THIS - IN THIS CASE, WE ARE MANIPULATING STATE DIRECTLY. THIS IS WRONG.this.state.persons[0].name = 'Maximilian';
-    this.setState({
+    // DON'T DO THIS - IN THIS CASE, WE ARE MANIPULATING STATE DIRECTLY. THIS IS WRONG.personsState.persons[0].name = 'Maximilian';
+    setPersonsState({
       persons: [
         {name: 'Maximilian', age: 28},
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 27}
       ]
-    })
+    });
+  };
 
-  }
-
-
-  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button onClick ={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick ={switchNameHandler}>Switch Name</button>
+        <Person 
+          name={personsState.persons[0].name} 
+          age={personsState.persons[0].age} 
+        />
+        <Person 
+          name={personsState.persons[1].name} 
+          age={personsState.persons[1].age}>
+            My Hobbies: Racing
+        </Person>
+        <Person 
+          name={personsState.persons[2].name} 
+          age={personsState.persons[2].age}
+        />
   
       </div>
     );
@@ -41,6 +51,6 @@ class App extends Component {
     // We typically don't use this type of code:
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
-}
 
 export default App;
+
