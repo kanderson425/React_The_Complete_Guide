e8c5ce9d7342a02852eb21c7361a1a6d7d51212e
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-08 21:38:28 -0500

    Adds the Person.css file

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Person.css';
 
 //Using ES6 - this is how we will create this component using const
 //We will use the arrow function courtesy of ES6, this is better form
@@ -6,7 +7,7 @@ import React from 'react';
 const person = (props) => {
 
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
