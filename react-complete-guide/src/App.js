970034344e3c1bd92e49248bb608037532dcd9e5
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-13 09:58:04 -0500

    Adds this.state.person.map() method to render the entire persons array dynamically as a list

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -117,7 +117,13 @@ class App extends Component {
       if (this.state.showPersons) {
         persons = (
           <div>
-          <Person 
+            {this.state.persons.map(person => {
+              return <Person 
+              name={person.name} 
+              age={person.age}/>
+
+            })}
+          {/* <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}/>
           <Person 
@@ -128,7 +134,7 @@ class App extends Component {
           </Person>
           <Person 
             name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}/>
+            age={this.state.persons[2].age}/> */}
         </div> 
         );
       }
