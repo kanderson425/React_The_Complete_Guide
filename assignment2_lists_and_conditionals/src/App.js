f4b8d80cace03802b7acc10752156719581e876e
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-16 18:49:42 -0500

    Adds colored border to person component

--- assignment2_lists_and_conditionals/src/App.js
+++ assignment2_lists_and_conditionals/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Assignment2: Lists & Conditionals</h1> 
-    </div>
-  );
-}
+//Class Based Approach
+class App extends Component {
+  state = {}
 
+
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Assignment2: Lists & Conditionals</h1> 
+        <input type="text" value="Insert Text Here" />
+      </div>
+    );
+  }
+
+};
 export default App;
