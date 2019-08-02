ce5d53567f795dfac5f25c555f7a214e6f1a068e
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-02 17:25:52 -0500

    Adds bold fonts to the person component(s)

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -122,15 +122,6 @@ class App extends Component {
   }
   
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    }
-
       let persons = null;
       let btnClass = 'button';
 
